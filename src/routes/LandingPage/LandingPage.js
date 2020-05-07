13e7174b0fd517973f8bb8398c02ef3851985aea
izaacAbrams <izaac.abrams25@gmail.com>
2020-05-06 20:18:02 -0500

    fixed error on landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -4,12 +4,11 @@ import "./LandingPage.css";
 export default function LandingPage() {
   return (
     <div className="LandingPage">
-      <h1 className="LandingPage__title">TimeSpace</h1>
+      <h1 className="LandingPage__title">TimeSpaces</h1>
       <p className="LandingPage__desc">
         TimeSpaces is the ideal tool for small business needs. It is easy to set
         up and takes all the work out of putting together your schedule!
       </p>
-      <button>Learn more</button>
       <button>
         <a href="/signup">Get Started</a>
       </button>
