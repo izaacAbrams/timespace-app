5104c5b7bc71114ff3645426f72545ad4e5f67b6
izaacAbrams <izaac.abrams25@gmail.com>
2020-05-25 16:13:03 -0500

    added hero for landing page, styled nav

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -6,6 +6,7 @@ export default function LandingPage() {
   return (
     <div className="LandingPage">
       <h1 className="LandingPage__title">TimeSpaces</h1>
+
       <p className="LandingPage__desc">
         TimeSpaces is the ideal tool for small business needs. It is easy to set
         up and takes all the work out of putting together your schedule! <br />
